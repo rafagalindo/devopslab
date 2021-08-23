6abc5901a6fa02ae510d1a0229a988ed0435b2db
Vagrant User <vagrant@multicloud>
2021-08-23 11:18:25 -0300

    Entrega DevSecOps

--- app.py
+++ app.py
@@ -4,9 +4,10 @@ from flask_wtf.csrf import CSRFProtect
 app = Flask(__name__)
 
 csrf = CSRFProtect(app)                                                                                                                           
-
 @app.route("/")
 def pagina_inicial():
     return "Laboratorio Pipeline DevOps"
 if __name__ == '__main__':
     app.run(debug=True)
+
+
